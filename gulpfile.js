195ce5ba24b540ccc9046f38aee1addbf99f2de3
Kunal Gupta <kunalgupta2k12@gmail.com>
2016-10-25 00:31:24 +0530

    Added a task to gulpfile.js for compiling scss every time we hit save

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+
+// Styles task
+// Compiles Scss
 gulp.task('sass', function () {
     return gulp.src('scss/styles.scss')
                .pipe(sass())
                .pipe(gulp.dest('css'))
+});
+
+
+// Watch task
+// Watches Scss files
+gulp.task('watch', function () {
+    return gulp.watch('scss/*.scss',['sass']);
+               
 });
 
